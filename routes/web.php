01cff5596fea1c8786bfb5adbd60482fca65b2d8
dev <genuinelycool@gmail.com>
2023-06-18 18:08:51 +0545

    fix route named home

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,13 @@ Route::get('/', function () {
         'posts' => Post::latest()->get(),
         'categories' => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', [
         'post' => $post
     ]);
-})->name('home');
+});
 
 Route::get('categories/{category:slug}', function (Category $category){
     return view('posts', [
