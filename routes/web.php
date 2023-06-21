d687f272cf1b88263eedadbfa24a532715599d1f
dev <genuinelycool@gmail.com>
2023-06-21 12:32:09 +0545

    author filtering

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('authors/{author:username}', function(User $author){
-    return view('posts', [
-        'posts' => $author->posts
-        // 'categories' => Category::all()
-    ]);
-});
+// Route::get('authors/{author:username}', function(User $author){
+//     return view('posts.index', [
+//         'posts' => $author->posts
+//         // 'categories' => Category::all()
+//     ]);
+// });
