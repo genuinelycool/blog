794525a38a76eb464504dcebe26ea9694af9a776
dev <genuinelycool@gmail.com>
2023-06-22 23:12:56 +0545

    laughably simple pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,8 +9,15 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index() {
+        // return Post::latest()->filter(
+        //     request(['search', 'category', 'author']))
+        // ->paginate();
+
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+            'posts' => Post::latest()->filter(
+                request(['search', 'category', 'author']))
+            ->paginate(6)->withQueryString()
+            // ->simplePaginate(6)
         ]);
     }
 
