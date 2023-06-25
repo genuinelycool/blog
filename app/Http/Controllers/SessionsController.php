46996f1ed9c74465d123f3005725b0bcffd45853
dev <genuinelycool@gmail.com>
2023-06-25 11:43:26 +0545

    code clean up, remove comments

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,40 +11,23 @@ class SessionsController extends Controller
     }
 
     public function store(){
-        // validate the request
         $attributes = request()->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
-        // attempt to authenticate and log in the user
-        // based on the provided credentials
         if(auth()->attempt($attributes)){
             session()->regenerate();
-            // session fixation
-
-            // redirect with a sucess flash message
             return redirect('/')->with('success', 'Welcome Back!');
         }
 
-        // auth failed.
         throw ValidationException::withMessages([
             'email' => 'Your provided credentials could not be verified.'
         ]);
-
-        // return back()
-        //     ->withInput()
-        //     ->withErrors(['email' => 'Your provided credentials could not be verified.']);
-
     }
 
     public function destroy(){
-        // ddd('log the user out');
-        // auth()->login();     // to login the user.
-        // auth()->check();        // check to see if user is logged in.
-        // auth()->guest();        // do we have a guest at the moment.
-
-        auth()->logout();       // to log out the user.
+        auth()->logout();
 
         return redirect('/')->with('success', 'Goodbye!');
     }
