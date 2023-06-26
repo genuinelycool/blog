3196e325a1d4148a1abda0b3d5dc752c8b4d9d5e
dev <genuinelycool@gmail.com>
2023-06-26 13:46:10 +0545

    design the comment form

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post(){     // column name is post_id
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author(){   // foreign is author_id but its not so needs to override 
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -34,6 +34,10 @@ class Post extends Model
         });
     }
 
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
     public function category(){
         // hasOne, hasMany, belongsTo, belongsToMany
         return $this->belongsTo(Category::class);
