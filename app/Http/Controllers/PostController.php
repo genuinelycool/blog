ab53a3d8cab8f15f7f111768ee67ba661bc2d612
dev <genuinelycool@gmail.com>
2023-06-30 02:52:43 +0545

    limit access to only admins

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,14 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PostController extends Controller
 {
     public function index() {
-        // return Post::latest()->filter(
-        //     request(['search', 'category', 'author']))
-        // ->paginate();
-
         return view('posts.index', [
             'posts' => Post::latest()->filter(
                 request(['search', 'category', 'author']))
@@ -25,5 +22,22 @@ class PostController extends Controller
         return view('posts.show', [
             'post' => $post
         ]);
+    }
+
+    public function create() {
+        // if (auth()->guest()) {
+        //     // abort(403);
+        //     abort(Response::HTTP_FORBIDDEN);
+        // }
+
+        // if (auth()->user()->username !== 'dev') {
+        //     abort(Response::HTTP_FORBIDDEN);
+        // }
+
+        // if (auth()->user()?->username !== 'dev') {
+        //     abort(Response::HTTP_FORBIDDEN);
+        // }
+
+        return view('posts.create');
     }
 }
