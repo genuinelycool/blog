86dbe070cb54837f245873cf17b167cedde7648a
dev <genuinelycool@gmail.com>
2023-06-29 20:11:33 +0545

    code clean up, remove comments, use php 8 onwards

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -8,16 +8,6 @@ use MailchimpMarketing\ApiClient;
 
 class Newsletter
 {
-        // protected ApiClient $client;
-        // protected string $foo;
-        // protected $client;
-        // protected $foo;
-
-        // public function __construct(ApiClient $client)
-        // public function __construct(ApiClient $client, string $foo)
-        // php8
-
-    // public function __construct(protected ApiClient $client, protected string $foo)
     public function __construct(protected ApiClient $client)
     {
         //
@@ -26,23 +16,11 @@ class Newsletter
 
     public function subscribe (string $email, string $list = null) {
 
-        // $list ??= config('services.mailchimp.lists.subscribers');   //null safe assignment operator
-        if (!isset($list)) {
-            $list = config('services.mailchimp.lists.subscribers');
-        }
-
-        // return $this->client()->lists->addListMember($list, [
+        $list ??= config('services.mailchimp.lists.subscribers');   //null safe assignment operator
+        
         return $this->client->lists->addListMember($list, [
             'email_address' => $email,
             'status' => 'subscribed'
         ]);
     }
-
-    // protected function client() {
-    //     // return (new ApiClient())->setConfig([       // inline gareko
-    //     return $this->client->setConfig([       // inline gareko
-    //         'apiKey' => config('services.mailchimp.key'),
-    //         'server' => 'us8'
-    //     ]);
-    // }
 }
